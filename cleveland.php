ebae1d0221028705bc0bd4ee540f165c931e53a3
juangrios <56139762+juangrios@users.noreply.github.com>
2019-12-17 10:30:09 -0300

    SD
    SD

--- cleveland.php
+++ cleveland.php
@@ -1,10 +1,10 @@
 <?php  
 
 // Llamando a los campos
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$telefono = $_POST['telefono'];
-$mensaje = $_POST['mensaje'];
+$nombre = $_GET['nombre'];
+$correo = $_GET['correo'];
+$telefono = $_GET['telefono'];
+$mensaje = $_GET['mensaje'];
 
 // Datos para el correo
 $destinatario = "juagrios@yahoo.com.ar";
